5348bd07f6822f9694638462249d7c55c874de83
Sterling Hopkins <sterlingxhopkins@gmail.com>
2022-03-14 23:19:04 -0400

    Added Player and Game Classes; created displayRules method.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+class Game {
+
+    constructor(){
+        this.rules = "Rules";
+        this.playerOne = new Player("Sterling");
+        this.playerTwo = new Player("Ace");
+    }
+    displayRules(){
+        console.log(this.rules);
+    }
+}
+
+class Player {
+
+    constructor(name){
+        this.options = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+        this.choice = null;
+        this.name = name;
+    }
+}
+let game = new Game();
+game.displayRules();
